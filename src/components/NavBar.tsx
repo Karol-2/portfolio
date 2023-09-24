4df7d56ae071923f6251bbecd056c2623aa0cc0b
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-09-24 16:14:49 +0200

    added navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,11 +4,12 @@ import "./NavBar.scss"
 
 export function NavBar () {
   return (
-    <nav>
+    <nav className='nav-bar'>
       <ul>
         <li className='nav-element'>Home</li>
         <li className='nav-element'>Education</li>
         <li className='nav-element'>Projects</li>
+        <li className='nav-element'> About Me</li>
       </ul>
     </nav>
   );
