295b88619824481b888a1c7df291f22ea3648635
Karol Krawczykiewicz <97386279+Karol-2@users.noreply.github.com>
2023-09-24 22:00:54 +0200

    changed folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import AboutMe from "./components/AboutMe";
-import Education from "./components/Education";
-import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Projects from "./components/Projects";
+import AboutMe from "./pages/AboutMe";
+import Education from "./pages/Education";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavBar />
-      <div className="page-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/about-me" element={<AboutMe />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/portfolio" element={<Home />} />
-        </Routes>
+        <div className="page-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/about-me" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/portfolio" element={<Home />} />
+          </Routes>
         </div>
       </div>
     </BrowserRouter>
